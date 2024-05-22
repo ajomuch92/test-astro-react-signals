a759a7cc892aeba5aadef2488383ed5eba985b6a
Aarón J. Montes <32050436+ajomuch92@users.noreply.github.com>
2024-05-21 21:05:58 -0600

    feat: 🎸 Creating test component for signals and state

---
+++ src/components/Counters.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { signal } from "@preact/signals-react";
+
+export default function Counters() {
+    const [counter1, setCounter1] = useState(0);
+    const counter2 = signal(0)
+
+    return (
+        <div>
+            <button onClick={() => setCounter1((val) => val + 1)}>
+                Counter 1: { counter1 }
+            </button>
+            <button onClick={() => counter2.value += 1}>
+                Counter 2: { counter2.value }
+            </button>
+        </div>
+    )
+}
